e80640d084db18ffaa41156eee6d3c2dc1a541b3
Munsi Abdul Al Mamun <mdabdulalmamoon@gmail.com>
2022-04-20 02:50:38 +0800

    Add project files.

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcesserTest.cs
@@ -0,0 +1,40 @@
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcesserTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        {
+            //Arrange
+            var request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2022, 04, 20)
+            };
+            var processor = new RoomBookingRequestProcesser();
+
+            //Acct
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+            /*Assert.NotNull(result);
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);*/
+
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(request.FullName);
+            result.Email.ShouldBe(request.Email);
+            result.Date.ShouldBe(request.Date);
+
+
+        }
+    }
+}
