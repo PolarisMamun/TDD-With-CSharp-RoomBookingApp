6e0e63200a7ca0ca52bcec1f38f80b98a9e0b75b
Munsi Abdul Al Mamun <mdabdulalmamoon@gmail.com>
2022-04-21 01:37:11 +0800

    Finished Unit Testing Application Core

--- RoomBookingApp.Core/Processors/RoomBookingRequestProcesser.cs
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcesser.cs
@@ -1,17 +1,55 @@
-﻿using RoomBookingApp.Core.Models;
+﻿using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Core.Domain;
+using RoomBookingApp.Core.Enums;
+using RoomBookingApp.Core.Models;
 using System;
+using System.Linq;
 
 namespace RoomBookingApp.Core.Processors
 {
     public class RoomBookingRequestProcesser
     {
-        public RoomBookingRequestProcesser()
+        private readonly IRoomBookingService _roomBookingService;
+
+        public RoomBookingRequestProcesser(IRoomBookingService roomBookingService)
         {
+            this._roomBookingService = roomBookingService;
         }
 
         public RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
         {
-            return new RoomBookingResult
+            if(bookingRequest == null)
+            {
+                throw new ArgumentNullException(nameof(bookingRequest));
+            }
+
+            var availableRooms = _roomBookingService.GetAvailableRooms(bookingRequest.Date);
+            var result = CreateRoomBookingObject<RoomBookingResult>(bookingRequest);
+
+            if (availableRooms.Any())
+            {
+                var room = availableRooms.First();
+                var roomBooking = CreateRoomBookingObject<RoomBooking>(bookingRequest);
+                roomBooking.RoomId = room.Id;
+                _roomBookingService.Save(roomBooking);
+
+                result.RoomBookingId = roomBooking.Id;
+                result.Flag = BookingResultFlag.Success;
+
+            }
+            else
+            {
+                result.Flag = BookingResultFlag.Failure;
+
+            }
+
+            return result;
+        }
+
+        private static TRoomBooking CreateRoomBookingObject<TRoomBooking>(RoomBookingRequest bookingRequest) where TRoomBooking 
+            : RoomBookingBase,new()
+        {
+            return new TRoomBooking
             {
                 FullName = bookingRequest.FullName,
                 Date = bookingRequest.Date,
