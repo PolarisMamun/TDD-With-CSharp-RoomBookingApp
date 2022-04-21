421011b0e007e0028a5ec8663bc4073780bbe0d7
Munsi Abdul Al Mamun <mdabdulalmamoon@gmail.com>
2022-04-21 15:28:18 +0800

    Added Integration Tests For Entity Framework Core

---
+++ RoomBookingApp.Domain/Room.cs
@@ -0,0 +1,13 @@
+﻿using RoomBookingApp.Domain.BaseModels;
+using System.Collections.Generic;
+
+namespace RoomBookingApp.Domain
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<RoomBooking> RoomBooking { get; set; }
+    }
+}
---
+++ RoomBookingApp.Persistence/Repositories/RoomBookingService.cs
@@ -0,0 +1,36 @@
+﻿using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingApp.Persistence.Repositories
+{
+    public class RoomBookingService : IRoomBookingService
+    {
+        private readonly RoomBookingAppDbContext _context;
+
+        public RoomBookingService(RoomBookingAppDbContext context)
+        {
+            this._context = context;
+        }
+        public IEnumerable<Room> GetAvailableRooms(DateTime date)
+        {
+            /*var unAvailableRooms = _context.RoomBookings.Where(q => q.Date == date).Select(q => q.Room).ToList();
+
+            var availableRooms = _context.Rooms.Where(q => unAvailableRooms.Contains(q.Id) == false).ToList();*/
+
+            var availableRooms = _context.Rooms.Where(q => !q.RoomBooking.Any(x=> x.Date == date)).ToList();
+
+            return availableRooms;
+        }
+
+        public void Save(RoomBooking roomBooking)
+        {
+            _context.Add(roomBooking);
+            _context.SaveChanges();
+        }
+    }
+}
